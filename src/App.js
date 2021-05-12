2049e7e356b09a49a07df1d6f3a675d4797dc646
Ashish-AVS <avsashish9@gmail.com>
2021-05-12 18:01:34 +0530

    heroku incoming

--- src/App.js
+++ src/App.js
@@ -17,13 +17,14 @@ function App() {
     axios.get("https://raw.githubusercontent.com/bitsacm/ps1data/master/src/data/ps1_data.json")
     .then(res => setData(res.data));
   }, [])
-
+  const branch = ["ANY", "B1", "B2", "B3", "B4", "B5", "AA", "A1", "A2", "A3", "A4", "A5", "A7", "A8", "C6"];
+  const industry = ["ELECTRONICS", "FINANCE AND MGMT", "MECHANICAL", "CHEMICAL"," CEMENT", "INFRASTRUCTURE", "IT", "COMPUTER SCIENCE", "HEALTH CARE", "GOVT RESEARCH LAB", "STEEL", "OTHERS"];
   return (
     <div className="App">
      <Layout>
        <Header className="header">  
         <h1 style={{color:"#fffbe6"}}>PS1 HELPER</h1>
-        <Menu theme="dark" mode="horizontal">
+        <Menu mode="horizontal">
           <Menu.Item key="1">nav 1</Menu.Item>
           <Menu.Item key="2">nav 2</Menu.Item>
           <Menu.Item key="3">nav 3</Menu.Item>
@@ -31,52 +32,35 @@ function App() {
        </Header>
        <Layout>
          <Sider width={300}>
-         {/* <Menu
-          mode="inline"
-          style={{ height: '100%', borderRight: 0 }}
-          >
-            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-              <Menu.Item key="1">option1</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
-          </SubMenu>
-          </Menu> */}
-          <Layout>
-          <Card title="FILTER BY BRANCH">
-          <Tag color="magenta">magenta</Tag>
-              <Tag color="red">red</Tag>
-              <Tag color="volcano">volcano</Tag>
-              <Tag color="orange">orange</Tag>
-              <Tag color="gold">gold</Tag>
-              <Tag color="lime">lime</Tag>
-              <Tag color="green">green</Tag>
-              <Tag color="cyan">cyan</Tag>
-              <Tag color="blue">blue</Tag>
-              <Tag color="geekblue">geekblue</Tag>
-              <Tag color="purple">purple</Tag>    
+    
+          <Card title="FILTER BY BRANCH" >
+             <Tag color="magenta" style={{marginBottom:"10px"}}>AA</Tag>
+              <Tag color="red">A1</Tag>
+              <Tag color="volcano">A2</Tag>
+              <Tag color="orange">A3</Tag>
+              <Tag color="gold">A4</Tag>
+              <Tag color="lime">A7</Tag>
+              <Tag color="green">A8</Tag>
+              <Tag color="cyan">B1</Tag>
+              <Tag color="blue">B2</Tag>
+              <Tag color="geekblue">B3</Tag>
+              <Tag color="purple">B5</Tag>     
           </Card> 
 
           <Card title="FILTER BY INDUSTRY">
-             <Tag color="magenta-inverse">magenta</Tag>
-            <Tag color="red-inverse">red</Tag>
-            <Tag color="volcano-inverse">volcano</Tag>
-            <Tag color="orange-inverse">orange</Tag>
+            <Tag color="magenta-inverse" style={{marginBottom:"10px"}}>ELECTRONICS</Tag>
+            <Tag color="red-inverse">FINANCE AND MGMT</Tag>
+            <Tag color="volcano-inverse">MECHANICAL</Tag>
+            <Tag color="orange-inverse" style={{marginBottom:"10px"}}>CHEMICAL</Tag>
             <Tag color="gold-inverse">gold</Tag>
-            <Tag color="lime-inverse">lime</Tag>
-            <Tag color="green-inverse">green</Tag>
+            <Tag color="lime-inverse" style={{marginBottom:"10px"}}>lime</Tag>
+            <Tag color="green-inverse" style={{marginBottom:"10px"}}>green</Tag>
             <Tag color="cyan-inverse">cyan</Tag>
             <Tag color="blue-inverse">blue</Tag>
             <Tag color="geekblue-inverse">geekblue</Tag>
             <Tag color="purple-inverse">purple</Tag>      
           </Card> 
-          </Layout>
+          
          </Sider>
          <Content>
           <Search placeholder="Eg: Machine Learning"  width = {300} size="large" enterButton prefix={<SearchOutlined/>} />
