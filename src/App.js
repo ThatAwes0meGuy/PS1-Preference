943bfdd53a91a705e93ed726d7f8bc5c11d9539b
Ashish-AVS <avsashish9@gmail.com>
2021-05-12 17:39:26 +0530

    Display looks cool

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
+import {useState, useEffect} from 'react';
 import { Input, Space, Layout } from 'antd';
-import { Menu, Breadcrumb, Card, Badge  } from 'antd';
+import { Menu, Breadcrumb, Card, Badge,  Table, Tag, Divider } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined, SearchOutlined } from '@ant-design/icons';
-
+import axios from 'axios';
+import MyData from './MyData'
 
 function App() {
   const { Search } = Input;
   const { Header, Footer, Sider, Content } = Layout;
   const { SubMenu } = Menu;
+  const [data, setData] = useState([]);
+  //https://raw.githubusercontent.com/bitsacm/ps1data/master/src/data/ps1_data.json
 
+  useEffect(() => {
+    axios.get("https://raw.githubusercontent.com/bitsacm/ps1data/master/src/data/ps1_data.json")
+    .then(res => setData(res.data));
+  }, [])
 
   return (
     <div className="App">
      <Layout>
-       <Header className="header">
-       
+       <Header className="header">  
+        <h1 style={{color:"#fffbe6"}}>PS1 HELPER</h1>
         <Menu theme="dark" mode="horizontal">
           <Menu.Item key="1">nav 1</Menu.Item>
           <Menu.Item key="2">nav 2</Menu.Item>
@@ -40,52 +48,39 @@ function App() {
             <Menu.Item key="12">option12</Menu.Item>
           </SubMenu>
           </Menu> */}
-          <Card title="FILTER BY INDUSTRY">
-          
-            <Space direction="vertical">
-            <Space direction="horizontal">
-              <Badge count={"IT"}/>
-              <Badge count={"ELECTRONICS"}/>
-              <Badge count={"ELECTRONICS"}/>
-            </Space>
-            <Space direction="horizontal">
-              <Badge count={"IT"}/>
-              <Badge count={"ELECTRONICS"}/>
-              <Badge count={"ELECTRONICS"}/>
-            </Space>
-            <Space direction="horizontal">
-              <Badge count={"IT"}/>
-              <Badge count={"ELECTRONICS"}/>
-              <Badge count={"ELECTRONICS"}/>
-            </Space>
-            </Space>
-          
-          </Card>
+          <Layout>
           <Card title="FILTER BY BRANCH">
-          <Space direction="vertical">
-            <Space direction="horizontal">
-              <Badge count={"IT"}/>
-              <Badge count={"ELECTRONICS"}/>
-              <Badge count={"ELECTRONICS"}/>
-            </Space>
-            <Space direction="horizontal">
-              <Badge count={"IT"}/>
-              <Badge count={"ELECTRONICS"}/>
-              <Badge count={"ELECTRONICS"}/>
-            </Space>
-            <Space direction="horizontal">
-              <Badge count={"IT"}/>
-              <Badge count={"ELECTRONICS"}/>
-              <Badge count={"ELECTRONICS"}/>
-            </Space>
-            </Space>
-          
-          </Card>
+          <Tag color="magenta">magenta</Tag>
+              <Tag color="red">red</Tag>
+              <Tag color="volcano">volcano</Tag>
+              <Tag color="orange">orange</Tag>
+              <Tag color="gold">gold</Tag>
+              <Tag color="lime">lime</Tag>
+              <Tag color="green">green</Tag>
+              <Tag color="cyan">cyan</Tag>
+              <Tag color="blue">blue</Tag>
+              <Tag color="geekblue">geekblue</Tag>
+              <Tag color="purple">purple</Tag>    
+          </Card> 
+
+          <Card title="FILTER BY INDUSTRY">
+             <Tag color="magenta-inverse">magenta</Tag>
+            <Tag color="red-inverse">red</Tag>
+            <Tag color="volcano-inverse">volcano</Tag>
+            <Tag color="orange-inverse">orange</Tag>
+            <Tag color="gold-inverse">gold</Tag>
+            <Tag color="lime-inverse">lime</Tag>
+            <Tag color="green-inverse">green</Tag>
+            <Tag color="cyan-inverse">cyan</Tag>
+            <Tag color="blue-inverse">blue</Tag>
+            <Tag color="geekblue-inverse">geekblue</Tag>
+            <Tag color="purple-inverse">purple</Tag>      
+          </Card> 
+          </Layout>
          </Sider>
          <Content>
-        
-              <Search placeholder="Eg: Machine Learning"  width = {300} size="large" enterButton prefix={<SearchOutlined/>} />
-
+          <Search placeholder="Eg: Machine Learning"  width = {300} size="large" enterButton prefix={<SearchOutlined/>} />
+           <MyData data={data}/>
          </Content>
        </Layout>
        <Footer></Footer>
---
+++ src/MyData.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import './App.css';
+import {useState, useEffect} from 'react';
+import { Input, Space, Layout } from 'antd';
+import { Menu, Breadcrumb, Card, Badge,  Table, Tag, Divider } from 'antd';
+
+export default function MyData(rawData) {
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: text => <a>{text}</a>,
+        },
+        {
+            title: 'Industry',
+            dataIndex: 'industry',
+            key: 'industry',
+        },
+        {
+            title: 'location',
+            dataIndex: 'location',
+            key: 'location',
+        },
+        
+        {
+            title: 'Branches',
+            dataIndex: 'branches',
+            key: 'branches',
+            render: branches => (
+                <>
+                  {branches.map(tag => {
+                      let color="pink"
+                     if(tag==="A7") color = "geekblue"
+                     if(tag==="A3") color = "gold"
+                     if(tag==="A8") color = "lime"
+                     if(tag==="AA") color = "green"
+                     if(tag==="Any") color = "cyan"
+                     if(tag==="A4") color = "orange"
+                     if(tag==="A1") color = "red"
+                     if(tag==="A2") color = "magenta"
+                    return (
+                      <Tag color={color} key={tag}>
+                        {tag.toUpperCase()}
+                      </Tag>
+                    );
+                  })}
+                </>
+            )
+        }
+        
+        ];
+
+    return (
+        <div>
+         <Table columns={columns} dataSource={rawData.data} />
+         {console.log(rawData.data)}
+        </div>
+    )
+}
