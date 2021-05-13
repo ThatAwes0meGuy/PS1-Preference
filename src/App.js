3e274e617fa000e80a4d8d53ca2c482461e51e7c
Ashish-AVS <avsashish9@gmail.com>
2021-05-13 11:44:23 +0530

    apply filter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
-import {useState, useEffect} from 'react';
-import { Input, Space, Layout } from 'antd';
-import { Menu,Card, Tag, Button} from 'antd';
-import { SearchOutlined, DownloadOutlined  } from '@ant-design/icons';
-import axios from 'axios';
-import Fuse from 'fuse.js';
-import MyData from './MyData';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Input, Space, Layout } from "antd";
+import { Menu, Card, Tag, Button } from "antd";
+import { SearchOutlined, DownloadOutlined } from "@ant-design/icons";
+import axios from "axios";
+import Fuse from "fuse.js";
+import MyData from "./MyData";
 import { CSVLink, CSVDownload } from "react-csv";
-
 
 function App() {
   const { Search } = Input;
@@ -16,71 +15,181 @@ function App() {
   const [data, setData] = useState([]);
   const [query, setQuery] = useState("");
   const [csv, setCsv] = useState([]);
+  const [branchQuery, setBranchQuery] = useState([]);
   //https://raw.githubusercontent.com/bitsacm/ps1data/master/src/data/ps1_data.json
 
   useEffect(() => {
-    axios.get("https://raw.githubusercontent.com/bitsacm/ps1data/master/src/data/ps1_data.json")
-    .then(res => setData(res.data));
-  }, [])
-  const fuse = new Fuse(data, {
-      keys: [
-        "name",
-        "industry",
-        "location",
-        "branches"
-      ]
+    axios
+      .get(
+        "https://raw.githubusercontent.com/bitsacm/ps1data/master/src/data/ps1_data.json"
+      )
+      .then((res) => setData(res.data));
+  }, []);
+
+  const options = {
+    // isCaseSensitive: false,
+    // includeScore: false,
+    // shouldSort: true,
+    // includeMatches: false,
+    // findAllMatches: false,
+    // minMatchCharLength: 1,
+    // location: 0,
+    threshold: 0.2,
+    // distance: 100,
+    // useExtendedSearch: false,
+    // ignoreLocation: false,
+    // ignoreFieldNorm: false,
+    tokenize: true,
+    matchAllTokens:true,
+    keys: [
+      "name", 
+      "industry", 
+      "location", 
+      "branches"
+    ]
+  };
+
+
+
+  // const fuse = new Fuse(data, options);
+  // const results = fuse.search(query);
+  // let dataResults = query ? results.map((result) => result.item) : data;
+
+  const branchFuse = new Fuse(data, 
+    {
+      keys: ["branches", "industry"],
+      tokenize: false,
+      threshold: 0.6
+    }
+    );
+   let branchResults;
+  branchQuery.map(el => {
+     branchResults = branchFuse.search(el)
   })
+  let branchDataResults = branchQuery.length ? branchResults.map((result) => result.item) : data;
 
+
+  const fuse = new Fuse(branchDataResults, options);
   const results = fuse.search(query);
-  const dataResults = query ? results.map(result => result.item): data;
-  const branch = ["ANY", "B1", "B2", "B3", "B4", "B5", "AA", "A1", "A2", "A3", "A4", "A5", "A7", "A8", "C6"];
-  const industry = ["ELECTRONICS", "FINANCE AND MGMT", "MECHANICAL", "CHEMICAL"," CEMENT", "INFRASTRUCTURE", "IT", "COMPUTER SCIENCE", "HEALTH CARE", "GOVT RESEARCH LAB", "STEEL", "OTHERS"];
-  const colors = ["magenta", "red", "orange", "gold", "lime", "green", "cyan", "blue", "geekblue", "purple"]
-
-  const queryHandler = ({currentTarget = {}}) => {
-    const {value} = currentTarget;
-    setQuery(value);
-  }
+  let dataResults = query ? results.map((result) => result.item) : branchDataResults;
   
-  const branchHandler = (branch) => {
-     console.log("HOW DO I DO THIS??")
+  
+
+  const branch = [
+    "ANY",
+    "B1",
+    "B2",
+    "B3",
+    "B4",
+    "B5",
+    "AA",
+    "A1",
+    "A2",
+    "A3",
+    "A4",
+    "A5",
+    "A7",
+    "A8",
+    "C6",
+  ];
+  const industry = [
+    "ELECTRONICS",
+    "FINANCE AND MGMT",
+    "MECHANICAL",
+    "CHEMICAL",
+    " CEMENT",
+    "INFRASTRUCTURE",
+    "IT",
+    "COMPUTER SCIENCE",
+    "HEALTH CARE",
+    "GOVT RESEARCH LAB",
+    "STEEL",
+    "OTHERS",
+  ];
+  const colors = [
+    "magenta",
+    "red",
+    "orange",
+    "gold",
+    "lime",
+    "green",
+    "cyan",
+    "blue",
+    "geekblue",
+    "purple",
+  ];
+
+  const queryHandler = ({ currentTarget = {} }) => {
+    const { value } = currentTarget;
+    setQuery(value);
+  };
+
+  const filterHandler = (branch) => {
+    // console.log(dataResults);
+    // setBranchQuery(prev => prev?[...prev, branch]:prev);
+    // dataResults = dataResults.map(el => {
+    //   if(el.branches){
+    //     if(el.branches.indexOf(branch)!==-1) return el;
+    //   }
+    // })
+    setBranchQuery(prev => [...prev, branch])
+    console.log(branchQuery)
+    // setQuery(prevQ => prevQ + " " + branch)
   }
   return (
     <div>
-     <Layout>
-     <Header className="header">  
+      <Layout>
+        <Header className="header">
+          <h1 style={{ color: "#fffbe6" }}>PS1 HELPER</h1>
+        </Header>
+        <Layout>
+          <Sider width={300} style={{ backgroundColor: "#fff" }}>
+            <Card title="FILTER BY BRANCH">
+              {branch.map((el, index) => (
+                <Tag
+                  key={index}
+                  onClick={() => filterHandler(el)}
+                  color={colors[index]}
+                  style={{ marginBottom: "10px" }}
+                >
+                  {el}
+                </Tag>
+              ))}
+            </Card>
 
-        <h1 style={{color:"#fffbe6"}}>PS1 HELPER</h1>
-
-       </Header>
-       <Layout>
-         <Sider width={300} style={{backgroundColor:"#fff"}}>
-          <Card title="FILTER BY BRANCH" >    
-             { branch.map( (el, index) => 
-                    <Tag key={index} onClick={(el) => branchHandler(el)} color={colors[index]} style={{marginBottom:"10px"}}>{el}</Tag>
-                 )}
-          </Card> 
-
-          <Card title="FILTER BY INDUSTRY">
-            { industry.map( (el, index) => 
-                    <Tag key={index} color={colors[index] + "-inverse"} style={{marginBottom:"10px"}}>{el}</Tag>
-                 )}  
-          </Card> 
+            <Card title="FILTER BY INDUSTRY">
+              {industry.map((el, index) => (
+                <Tag
+                  key={index}
+                  color={colors[index] + "-inverse"}
+                  style={{ marginBottom: "10px" }}
+                  onClick={() => filterHandler(el)}
+                >
+                  {el}
+                </Tag>
+              ))}
+            </Card>
             <Button type="primary" icon={<DownloadOutlined />} size="large">
-              <CSVLink data={dataResults}><span style={{color:"#fff"}}>Download as CSV</span></CSVLink>
+              <CSVLink data={dataResults}>
+                <span style={{ color: "#fff" }}>Download as CSV</span>
+              </CSVLink>
             </Button>
-            
-
-
-
-         </Sider>
-         <Content>
-          <Search placeholder="Eg: Machine Learning"  width = {300} size="large" enterButton prefix={<SearchOutlined/>} onChange={queryHandler}/>
-           <MyData data={dataResults}/>
-         </Content>
-       </Layout>
-       <Footer></Footer>
-     </Layout>
+          </Sider>
+          <Content>
+            <Search
+              placeholder="Eg: Machine Learning"
+              width={300}
+              size="large"
+              enterButton
+              prefix={<SearchOutlined />}
+              onChange={queryHandler}
+              value={query}
+            />
+            <MyData data={dataResults} />
+          </Content>
+        </Layout>
+        <Footer></Footer>
+      </Layout>
     </div>
   );
 }
--- src/MyData.js
+++ src/MyData.js
@@ -1,21 +1,21 @@
-import React, {useState} from 'react'
-import './App.css';
-import {Table, Tag, Modal} from 'antd';
+import React, { useState } from "react";
+import "./App.css";
+import { Table, Tag, Modal } from "antd";
 
 export default function MyData(rawData) {
-const [isModalVisible, setIsModalVisible] = useState(false);
-const [modalContent, setModalContent] = useState(["Notjing"]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalContent, setModalContent] = useState(["Sphagetti Code"]);
 
   const showModal = (text) => {
     setIsModalVisible(true);
-    rawData.data.map(el => {
-        if(el.name===text){
-            setModalContent(el.projects);
-            // console.log(el.projects)
-        }
-    })
+    rawData.data.map((el) => {
+      if (el.name === text) {
+        setModalContent(el.projects);
+        // console.log(el.projects)
+      }
+    });
     // setModalContent(text);
-    console.log(rawData)
+    console.log(rawData);
   };
 
   const handleOk = () => {
@@ -25,80 +25,80 @@ const [modalContent, setModalContent] = useState(["Notjing"]);
   const handleCancel = () => {
     setIsModalVisible(false);
   };
-  const displayModalContent = () => {
-      modalContent.map(el => modalContent)
-  }
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-            render: text => <span onClick={() => showModal(text)}>{text}</span>,
-        },
-        {
-            title: 'Industry',
-            dataIndex: 'industry',
-            key: 'industry',
-        },
-        {
-            title: 'location',
-            dataIndex: 'location',
-            key: 'location',
-        },
-        
-        {
-            title: 'Branches',
-            dataIndex: 'branches',
-            key: 'branches',
-            render: branches => (
-                <>
-                  {branches.map(tag => {
-                      let color="pink"
-                     if(tag==="A7") color = "geekblue"
-                     if(tag==="A3") color = "gold"
-                     if(tag==="A8") color = "lime"
-                     if(tag==="AA") color = "green"
-                     if(tag==="Any") color = "cyan"
-                     if(tag==="A4") color = "orange"
-                     if(tag==="A1") color = "red"
-                     if(tag==="A2") color = "magenta"
-                    return (
-                      <Tag color={color} key={tag}>
-                        {tag.toUpperCase()}
-                      </Tag>
-                    );
-                  })}
-                </>
-            )
-        }
-        
-        ];
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <span onClick={() => showModal(text)}>{text}</span>,
+    },
+    {
+      title: "Industry",
+      dataIndex: "industry",
+      key: "industry",
+    },
+    {
+      title: "location",
+      dataIndex: "location",
+      key: "location",
+    },
 
-    return (
+    {
+      title: "Branches",
+      dataIndex: "branches",
+      key: "branches",
+      render: (branches) => (
         <div>
-         <Modal title="Project Description" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-            {
-                <div>
-                    {modalContent.map((proj, index) => {
-                        let projKeys = Object.keys(proj);
-                        return(
-                            <div style={{textAlign:"left"}}>
-                                <Tag color="#2db7f5">#{index+1}</Tag>
-                               
-                                {projKeys.map(ele => 
-                                <div>
-                                <b>{ele.toUpperCase()}</b> : {proj[ele]}
-                                </div>
-                                )}
-                                <br></br>
-                             </div>
-                        )
-                    }
-                    )}
-                </div>
-            }
-         </Modal>
-         <Table columns={columns} dataSource={rawData.data}/>
+          {branches.map((tag) => {
+            let color = "pink";
+            if (tag === "A7") color = "geekblue";
+            if (tag === "A3") color = "gold";
+            if (tag === "A8") color = "lime";
+            if (tag === "AA") color = "green";
+            if (tag === "Any") color = "cyan";
+            if (tag === "A4") color = "orange";
+            if (tag === "A1") color = "red";
+            if (tag === "A2") color = "magenta";
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
         </div>
-    )
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <Modal
+        title="Project Description"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        {
+          <div>
+            {modalContent.map((proj, index) => {
+              let projKeys = Object.keys(proj);
+              return (
+                <div style={{ textAlign: "left" }}>
+                  <Tag color="#2db7f5">#{index + 1}</Tag>
+
+                  {projKeys.map((ele) => (
+                    <div>
+                      <b>{ele.toUpperCase()}</b> : {proj[ele]}
+                    </div>
+                  ))}
+                  <br></br>
+                </div>
+              );
+            })}
+          </div>
+        }
+      </Modal>
+      <Table columns={columns} dataSource={rawData.data} />
+    </div>
+  );
 }
